bada445af90f743eb436ecb0a0fe14266b209181
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-10-28 18:16:12 +0200

    add last snack

--- js-snacks-blocco-2/js-snack4/js/main.js
+++ js-snacks-blocco-2/js-snack4/js/main.js
@@ -1,3 +1,15 @@
 // Crea due array che hanno un numero di elementi diversi. Aggiungi elementi casuali all’array che ha meno elementi, fino a quando ne avrà tanti quanti l’altro.
 
 const arr1 = ["Eleonora", "Michela", "Andrea", "Francesco"];
+
+const arr2 = ["Paolo", "Luca", "Sara"];
+
+while (arr1.length !== arr2.length) {
+  if (arr1.length < arr2.length) {
+    arr1.push("ciao");
+  } else if (arr2.length < arr1.length) {
+    arr2.push("ciao");
+  }
+}
+
+console.log(arr1, arr2);
